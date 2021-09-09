276798b0cf4b858b8b3b6b879e86962eccc58688
onikolenko <onikolenko@alfabank.ru>
2021-09-09 20:55:10 +0300

    набросок

--- pet_store_assert.py
+++ pet_store_assert.py
@@ -1,6 +1,6 @@
 
 
 def assert_json_data(actual_data, expected_data):
-    """ обновляем id в ожидаемом json чтобы полностью сравнить """
+    """ обновляем id в ожидаемом json чтобы полностью сравнить запрос с ответом"""
     expected_data['id'] = actual_data['id']
     assert actual_data == expected_data
